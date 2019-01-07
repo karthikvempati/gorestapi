b3062b12548883e7811ac683ea9858f2ba0cda19
Karthik Vempati <karthikvempati@gmail.com>
2019-01-06 21:04:22 -0600

    Update longestpalindrome.go

--- dp/longestpalindrome.go
+++ dp/longestpalindrome.go
@@ -1,52 +1,61 @@
 func longestPalindrome(s string) string {
-    m := make(map[string]bool)
-    result := "" 
-    for i :=0;i<len(s)-1;i++ {
-        for j:=i;j<len(s);j++{
-            if(isPalindrome(i,j,s,m)){
-                c := s[i:j]
-                if(len(result) < len(c)){
-                    result = c    
-                }    
-            }
-        }
+	if len(s) <= 1 {
+		return s
+	}
+    
+    l := len(s) 
+    m := make([][]bool,l)
+    for i := 0;i<l;i++ {
+        m[i] = make([]bool,l)
     }
     
-    return result
+	result := ""
+    lr := 0
+    for i := 0; i < l-1; i++ {
+        for j := i+lr; j < l; j++ {
+			if isPalindrome(i, j, s, m) {
+				c := s[i : j+1]
+				if len(result) < len(c) {
+					result = c
+                    lr = len(result)
+				}
+			}
+		}
+	}
+
+	return result
 }
 
-func isPalindrome(i int,j int, s string, m map[string]bool) bool {
-    if i <= j {
+func isPalindrome(i int, j int, s string, m [][]bool) bool {
+	if i <= j {
+
+		if m[i][j] {
+            return m[i][j]
+        }
         
-    buffer1 := bytes.NewBufferString(strconv.Itoa(i))
-    buffer1.WriteString("$")
-    buffer1.WriteString(strconv.Itoa(j))
-    lkey :=  buffer1.String()    
-    _,ok := m[lkey];
-        
-    if ok {
-        return m[lkey]
-    }
-        a := i+1
-        b := j+1 
-        buffer := bytes.NewBufferString(strconv.Itoa(a))
-        buffer.WriteString("$")
-        buffer.WriteString(strconv.Itoa(b))
-        key :=  buffer.String()
-        subPalindrome := false
-        _,keyExists := m[key]
-        if(keyExists){
-            subPalindrome = m[key] 
+		a := i + 1
+		b := j - 1
+		
+        subPalindrome := false 
+		
+        if(a < 0 || b < 0 || a > b){
+            subPalindrome = true
         } else {
-            isPalindrome(a,b,s,m)
+            if m[a][b] {
+                subPalindrome = m[a][b]
+            } else {
+                subPalindrome = isPalindrome(a, b, s, m)
+            }
+        }
+
+		result := (s[i] == s[j]) && subPalindrome
+        
+        if(a >= 0 && b >= 0 && a <= b){
+            m[a][b] = result
         } 
-        
-        result := (s[i]==s[j]) && subPalindrome
-        
-        m[lkey] = result
-        
-        return result 
-    }
-    
-    return true
+
+		return result
+	}
+
+	return true
 }
